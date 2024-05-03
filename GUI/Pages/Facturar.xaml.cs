22fd8d42edb6c81923154a23f59fe3770382b445
Dagerdev <maro4df@gmail.com>
2024-05-03 00:17:30 -0500

    Combobox Alternative

--- GUI/Pages/Facturar.xaml.cs
+++ GUI/Pages/Facturar.xaml.cs
@@ -27,14 +27,62 @@ namespace GUI.Pages
 
 
 
+
         public Facturar()
         {
             InitializeComponent();
             clientes = new List<string> { "Cliente 1", "Cliente 2", "Cliente 3", "juan0", "julian", "carlos0", "camilo" };
-            cb.ItemsSource = clientes;
+            lstbox.ItemsSource = clientes;
+            txtbox.TextChanged += Tb_TextChanged;
+            txtbox.AddHandler(TextBox.MouseLeftButtonDownEvent, new MouseButtonEventHandler(TextBoxclick), true);
 
         }
 
+
+
+        private void Tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                var filteredList = clientes.Where(item => item.StartsWith(textBox.Text)).ToList();
+                lstbox.ItemsSource = filteredList;
+            }
+        }
+
+        private void lb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            if (listBox != null)
+            {
+                txtbox.Text = listBox.SelectedItem.ToString();
+            }
+            lstbox.Visibility = Visibility.Hidden;
+            ControlTemplate template = txtbox.Template;
+            Border border = (Border)template.FindName("borderbox", txtbox);
+            border.CornerRadius = new CornerRadius(5);
+        }
+
+
+        private void TextBoxclick(object sender, RoutedEventArgs e)
+        {
+            lstbox.Visibility = Visibility.Visible;
+            ControlTemplate template = txtbox.Template;
+            Border border = (Border)template.FindName("borderbox", txtbox);
+            border.CornerRadius = new CornerRadius(5,5,0,0);
+           
+
+        }
+
+        //private void lstboxclick(object sender, RoutedEventArgs e)
+        //{
+
+
+        //    txtbox.Text= lstbox.SelectedItem.ToString();
+        //    MessageBox.Show(lstbox.SelectedItem.ToString());
+        //    lstbox.Visibility = Visibility.Hidden;
+
+        //}
 
         private void cb_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -44,7 +92,9 @@ namespace GUI.Pages
                 comboBox.IsDropDownOpen = true;
             }
         }
+
+        
     }
-    }
+}
 
 
